7fd31479d5f62e1e6d2d6a9eb353392869650290
Chen Meyouhas <cmtk85@gmail.com>
2017-09-27 19:11:33 +0300

    First Commit

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const app = express()
 
-app.use(express.static('public'))
 
 app.listen(process.env.PORT || 8080, () => console.log('all ok'))
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Hello Chen!')
 })
